6e6730d96e522c213a2fd94a6312973832926ebe
Jordan-Pays-LMKSystem <jordan@lmksystem.fr>
2022-03-16 16:22:59 +0100

    responsive tablette ajouté + changement icon pour submit plus traduction des éléments écrits en français

--- P2_PAYS_code/js/general.js
+++ P2_PAYS_code/js/general.js
@@ -1,25 +1,28 @@
 function clickFiltres(id) {
-    let tabFiltres = document.getElementsByClassName("item_filtres")
-    let isActive = tabFiltres[id].classList.contains('is_active_filtres')
-    if(isActive){
-        tabFiltres[id].classList.remove("is_active_filtres")
-    }else{
-        tabFiltres[id].classList.add("is_active_filtres")
-    }
+    const tabFiltres = document.getElementsByClassName("item_filtres")
+    toggleActive('is_active_filtres',tabFiltres[id])
 }
 
 function clickNav(id) {
-    let tabNav = document.getElementsByClassName("element_nav")
-    let isActive = false
-    let i = 0
+    const tabNav = document.getElementsByClassName("element_nav")
     
     while(i<tabNav.length){
-        isActive = tabNav[i].classList.contains("element_nav_active")
-            if(i!=id && isActive){
-                tabNav[i].classList.remove("element_nav_active")
-            }else if(i==id){
-                tabNav[i].classList.add("element_nav_active")
-            }
+
+        if(i==id){
+
+        }
+            
         i++
+    }
+}
+
+function toggleActive(classActive,dom) {
+    let tabClassList = dom.classList
+    const isActive = tabClassList.contains(classActive)
+
+    if(isActive){
+        tabClassList.remove(classActive)
+    }else{
+        tabClassList.add(classActive)
     }
 }
