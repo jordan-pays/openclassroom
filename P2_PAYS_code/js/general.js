eca3c5f7b91dd359e735b9ac4c225c4385af320f
pays jordan <jordan@lmksystem.fr>
2022-03-16 08:32:14 +0100

    oublie push, push partie popularité, push partie Activite, push partie Footer, push changement de la session de mentorat du 09/02/22

--- P2_PAYS_code/js/general.js
+++ P2_PAYS_code/js/general.js
@@ -1,45 +1,25 @@
 function clickFiltres(id) {
     let tabFiltres = document.getElementsByClassName("item_filtres")
-    let tabClass = tabFiltres[id].classList
-    let estActif = false
-    let i=0
-    while(!estActif && i<tabClass.length){
-        if(tabClass[i]=="is_active_filtres"){
-            estActif=true
-        }
-        i++
-    }
-    if(estActif){
-        tabFiltres[id].className = tabFiltres[id].className.replace(" is_active_filtres","")
+    let isActive = tabFiltres[id].classList.contains('is_active_filtres')
+    if(isActive){
+        tabFiltres[id].classList.remove("is_active_filtres")
     }else{
-        tabFiltres[id].className +=" is_active_filtres"
+        tabFiltres[id].classList.add("is_active_filtres")
     }
 }
 
 function clickNav(id) {
     let tabNav = document.getElementsByClassName("element_nav")
-    let estActif = false
+    let isActive = false
     let i = 0
     
-    while(i<tabNav.length && !estActif){
-        let tabClass = tabNav[i].classList
-        let j = 0
-        while(j<tabClass.length && !estActif){
-            if(i==id){
-                if(tabClass[j]=="element_nav_active"){
-                    estActif=true
-                }
-            }else{
-                if(tabClass[j]=="element_nav_active"){
-                    tabNav[i].className = tabNav[i].className.replace(" element_nav_active","")
-                }
+    while(i<tabNav.length){
+        isActive = tabNav[i].classList.contains("element_nav_active")
+            if(i!=id && isActive){
+                tabNav[i].classList.remove("element_nav_active")
+            }else if(i==id){
+                tabNav[i].classList.add("element_nav_active")
             }
-            j++
-        }
         i++
-    }
-
-    if(!estActif){
-        tabNav[id].className += " element_nav_active"
     }
 }
