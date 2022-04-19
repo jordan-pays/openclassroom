99de7d97327ff194668e30251d01c9043f45ecac
Jordan-Pays-LMKSystem <jordan@lmksystem.fr>
2022-04-19 17:32:05 +0200

    changement nom P2 + début P3

---
+++ Pays_Jordan_1_code_032022/js/general.js
@@ -0,0 +1,42 @@
+function clickfilters(id) {
+    const tabFilters = document.getElementsByClassName("item_filters")
+    toggleActive('is_active_filters',tabFilters[id])
+}
+
+function clickNav(id) {
+    const tabNav = document.getElementsByClassName("element_nav")
+    let i=0
+
+    while (i<tabNav.length) { 
+        const isActiveBis = isActive("element_nav_active",tabNav[i])
+        if ( isActiveBis && i!=id) {
+            removeActive("element_nav_active",tabNav[i])
+        }else if(!isActiveBis && i==id){
+            addActive("element_nav_active",tabNav[i])
+        }
+
+        i++
+    }
+
+    addActive("element_nav_active",tabNav[id])
+}
+
+function toggleActive(classActive,dom) {
+    if (isActive(classActive,dom)) {
+        removeActive(classActive,dom)
+    } else {
+        addActive(classActive,dom)
+    }
+}
+
+function isActive(classActive,dom){
+    return dom.classList.contains(classActive)
+}
+
+function removeActive(classActive,dom){
+    dom.classList.remove(classActive)
+}
+
+function addActive(classActive,dom){
+    dom.classList.add(classActive)
+}
